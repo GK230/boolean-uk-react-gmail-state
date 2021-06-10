fb022687d6e31db60bf258d8c586d30003641790
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-06-10 16:43:39 +0100

    Working on search bar

--- src/App.js
+++ src/App.js
@@ -5,24 +5,64 @@ import initialEmails from './data/emails'
 import './App.css'
 
 import { useState } from 'react'
+import { flattenDiagnosticMessageText } from 'typescript'
 
 function App() {
   // Use initialEmails for state
   // console.log(initialEmails)
 
   const [emails, setEmails] = useState(initialEmails)
-  console.log(emails)
+  const [pleaseHideReadEmails, setPleaseHideReadEmails] = useState(false)
+  const [currentTab, setCurrentTab] = useState('inbox')
 
   function toggleRead(targetEmail) {
-    const updatedEmails = emails.map(function(email) {
+    const updatedEmails = emails.map(function (email) {
       if (email.id === targetEmail.id) {
-        email.read = !email.read
-        console.log(email)
+        return { ...email, read: !email.read }
       }
       return email
     })
     setEmails(updatedEmails)
   }
+
+  function toggleStarred(targetEmail) {
+    const updatedEmails = emails.map(function (email) {
+      if (email.id === targetEmail.id) {
+        return { ...email, starred: !email.starred }
+      }
+    })
+    setEmails(updatedEmails)
+  }
+
+  function hideReadEmails() {
+    setPleaseHideReadEmails(!pleaseHideReadEmails)
+  }
+
+  function displayStarred() {
+    let currentTab = 'starred'
+    setCurrentTab(currentTab)
+  }
+
+  function displayInbox() {
+    let currentTab = 'inbox'
+    setCurrentTab(currentTab)
+  }
+
+  let emailsToRender = emails
+  if (currentTab === 'starred')
+    emailsToRender = emailsToRender.filter(email => email.starred)
+  if (pleaseHideReadEmails)
+    emailsToRender = emailsToRender.filter(email => !email.read)
+  // if (search !== '')
+  //   emailsToRender = emailsToRender.filter(email => {
+  //     return (
+  //       email.title.toLowerCase().includes(search.toLowerCase()) ||
+  //       email.sender.toLowerCase().includes(search.toLowerCase())
+  //     )
+  //   })
+
+  const unreadCount = emails.filter(email => !email.read).length
+  const starredCount = emails.filter(email => email.starred).length
 
   return (
     <div className="app">
@@ -31,17 +71,21 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+            onClick={() => {
+              displayInbox()
+            }}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{unreadCount}</span>
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+            onClick={() => {
+              displayStarred()
+            }}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{starredCount}</span>
           </li>
 
           <li className="item toggle">
@@ -49,17 +93,22 @@ function App() {
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
+              checked={pleaseHideReadEmails}
+              onChange={() => {
+                hideReadEmails()
+              }}
             />
           </li>
         </ul>
       </nav>
       <main className="emails">
-        <ul className="inbox-list">
-          {emails.map(function (email) {
+        <ul>
+          {emailsToRender.map(function (email) {
             return (
-              <li key={email.id} className="email">
+              <li
+                key={email.id}
+                className={`email ${email.read ? 'read' : 'unread'}`}
+              >
                 <input
                   type="checkbox"
                   checked={email.read}
@@ -67,7 +116,15 @@ function App() {
                     toggleRead(email)
                   }}
                 />
-                <input type="checkbox" className="star-checkbox" />
+                <input
+                  type="checkbox"
+                  className="star-checkbox"
+                  checked={email.starred}
+                  onChange={function () {
+                    toggleStarred(email)
+                  }}
+                />
+
                 <p>{email.sender}</p>
                 <p className="title">{email.title}</p>
               </li>
